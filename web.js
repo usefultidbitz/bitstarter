04cc653e10577a98384a7fe6d94452f7414a165e
Useful Tidbits <usefultidbitz@gmail.com>
2013-07-10 01:55:43 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var outString = fs.readFileSync('index.html').toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(outString);
 });
 
 var port = process.env.PORT || 5000;
